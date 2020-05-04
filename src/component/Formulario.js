12ecfcdd860deefbcfc5bc7e461806f24ba38be6
Leonides Muguercia Tellez <leonimuguercia5@gmail.com>
2020-05-04 18:15:53 +0200

    Validando el formulario

--- src/component/Formulario.js
+++ src/component/Formulario.js
@@ -9,17 +9,47 @@ const Formulario = () => {
         hora: '',
         sintomas: ''
     });
+    
+    const [error, actualizarError] = useState(false);
 
     const handleChange = (e) => {
         actualizarCita({
             ...cita,
             [e.target.name]: e.target.value
         })
+    };
+
+    //Extraer los valores
+    const {mascota, propietario, fecha, hora, sintomas} = cita;
+
+    //Cuando el usuario presiona agregar cita
+    const submitCita = (e) => {
+        e.preventDefault();
+
+        //Validar el form
+        if(mascota.trim() ==='' || propietario.trim() ==='' || fecha.trim() ==='' || hora.trim() ==='' || sintomas.trim() ==='') {
+            actualizarError(true);
+            return;
+        } else {
+            actualizarError(false)
+        }
+
+        //Asignar un Id
+
+        //Crear la cita
+
+        //Reiniciar el form
     }
+    
+
     return ( 
         <Fragment>
+            {error ? <p className="alerta-error">Todos los campos son obligatorios</p> 
+            :  null}
             <h2>Crear Citas</h2>
-            <form>
+            <form
+                onSubmit={submitCita}
+            >
                 <label>Nombre Mascota</label>
                 <input
                     type="text"
@@ -27,6 +57,7 @@ const Formulario = () => {
                     className="u-full-width"
                     placeholder="Nombre Mascota"
                     onChange={handleChange}
+                    value={mascota}
                 />
                  <label>Nombre Dueño</label>
                 <input
@@ -35,6 +66,7 @@ const Formulario = () => {
                     className="u-full-width"
                     placeholder="Nombre Dueño de la Mascota"
                     onChange={handleChange}
+                    value={propietario}
                 />
                  <label>Fecha</label>
                 <input
@@ -42,6 +74,7 @@ const Formulario = () => {
                     name="fecha"
                     className="u-full-width"
                     onChange={handleChange}
+                    value={fecha}
                 />
                  <label>Hora</label>
                 <input
@@ -49,12 +82,16 @@ const Formulario = () => {
                     name="hora"
                     className="u-full-with"
                     onChange={handleChange}
+                    value={hora}
+
                 />
                 <label>Síntomas</label>
                 <textarea 
                     className="u-full-width"
                     name="sintomas"   
-                    onChange={handleChange} 
+                    onChange={handleChange}
+                    value={sintomas}
+ 
                 ></textarea>
                 <button
                     type="submit"
